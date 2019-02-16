6dd9d22eb893d3d772f15e4ee9fdc1442c85a6df
GCD <andy88566y@gmail.com>
2019-02-16 22:26:39 +0800

    add title unique true

--- db/migrate/20190216140747_add_title_constraints_to_mission.rb
+++ db/migrate/20190216140747_add_title_constraints_to_mission.rb
@@ -1,6 +1,6 @@
 class AddTitleConstraintsToMission < ActiveRecord::Migration[5.2]
   def up
-    change_column :missions, :title, :string, null: false
+    change_column :missions, :title, :string, null: false, unique: true
   end
 
   def down
