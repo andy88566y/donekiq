f0b1206b19665ef76b3bda0e4bb7c0a538bb7961
GCD <andy88566y@gmail.com>
2019-02-16 22:00:43 +0800

    add spec for Mission default scope

--- spec/models/mission_spec.rb
+++ spec/models/mission_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe Mission, type: :model do
     mission.valid?
     expect(mission.errors[:title]).to include("不能為空白")
   end
+
+  describe "default scope" do
+    let!(:mission_one) {Mission.create(title: "1", end_time: 1.minute.ago, priority: "Low")}
+    let!(:mission_two) {Mission.create(title: "2", end_time: 2.minute.ago, priority: "Low")}
+
+    it 'order by created_at desc' do
+      expect(Mission.all).to eq [mission_two, mission_one]
+    end
+  end
 end
