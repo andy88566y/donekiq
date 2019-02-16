8cefb9293a44eae26b8babbfaf72cee5ea1e12d1
GCD <andy88566y@gmail.com>
2019-02-16 21:59:23 +0800

    Mission加上 default scope

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -2,4 +2,5 @@ class Mission < ApplicationRecord
   validates :title, presence: true
   validates :end_time, presence: true
   validates_inclusion_of :priority, in: ["Urgent", "Medium", "Low"]
+  default_scope { order(created_at: :desc)}
 end
